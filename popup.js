c29c809c17d837e44c05d2dea835c2fe979b31d3
wutongwutong00 <wu-tong-apple@hotmail.com>
2023-12-02 00:18:47 -0500

    icons

--- popup.js
+++
@@ -1,7 +0,0 @@
-let clickedEl = document.getElementById('clickedTimes');
-let clickedCount = 0;
-
-chrome.storage.local.get(["clickedTimes"]).then((result) => {
-    clickedCount = result.clickedTimes || 0;
-    clickedEl.innerText = `Clicked ${clickedCount}`;
-  });
