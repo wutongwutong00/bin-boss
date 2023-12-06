7ab2b6e6b2ae420bfcd5aa0922b70cbc926ddb8d
kungn103 <kungn103@newschool.edu>
2023-12-06 16:33:45 -0500

    push again :(

--- scripts/content.js
+++ scripts/content.js
@@ -1,24 +1,3 @@
-function highlightSelection() {
-    var userSelect = window.getSelection();
-    console.log(userSelect);
-    
-    var anchorNode = userSelect.anchorNode;
-    if (anchorNode && anchorNode.nodeType !== 3) {
-        var nonTextLink = userSelect.anchorNode.baseURI;
-        console.log("heres a link to your selection:", nonTextLink);
-    } else if (userSelect.type === "Range") {
-        var selectedText = userSelect.toString();
-        console.log("you selected this:", selectedText);
-    }
-    var range = userSelect.getRangeAt(0);
-    var highlighted = document.createElement('span');
-    highlighted.className = 'highlighted';
-    highlighted.style.backgroundColor = 'yellow';
-    highlighted.style.color = 'black';
-    range.surroundContents(highlighted);
-    console.log(highlighted);
-}
-window.addEventListener('mouseup', highlightSelection);
 
 const initialBin = []; 
 const archiveBin = []; 
@@ -42,6 +21,28 @@ class Highlight {
         let year = date.getFullYear();
 
         let currentDate = `${day} ${month} ${year}`; 
+
+        function highlightSelection() {
+            var userSelect = window.getSelection();
+            console.log(userSelect);
+            
+            var anchorNode = userSelect.anchorNode;
+            if (anchorNode && anchorNode.nodeType !== 3) {
+                var nonTextLink = userSelect.anchorNode.baseURI;
+                console.log("heres a link to your selection:", nonTextLink);
+            } else if (userSelect.type === "Range") {
+                var selectedText = userSelect.toString();
+                console.log("you selected this:", selectedText);
+            }
+            var range = userSelect.getRangeAt(0);
+            var highlighted = document.createElement('span');
+            highlighted.className = 'highlighted';
+            highlighted.style.backgroundColor = 'yellow';
+            highlighted.style.color = 'black';
+            range.surroundContents(highlighted);
+            console.log(highlighted);
+        }
+        window.addEventListener('mouseup', highlightSelection);
 
         this.content = userSelect.toString();
         this.yourNotes = notes.innerText; 
